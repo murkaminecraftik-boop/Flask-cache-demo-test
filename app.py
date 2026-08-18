29fd4f3ed1e5d83ec248b73c785e77e79fd16783
Murad <murad.mirzazade.emil@gmail.com>
2026-08-18 16:09:01 +0400

    Added logging

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify, render_template
+import time
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 ideas_db = []
 
@@ -12,11 +15,21 @@ def index():
 
 @app.route('/api/ideas', methods=['GET', 'POST'])
 def manage_ideas():
+    start_time = time.time()
+
     if request.method == 'POST':
         data = request.get_json()
         if 'idea' in data and data['idea'].strip():
             ideas_db.append(data['idea'].strip())
+
+
+        duration = time.time() - start_time
+        app.logger.info(f"[БЕЗ КЭША] Добавлена новая идея | Время обработки (POST): {duration:.4f} сек")
+
         return jsonify({"status": "success"})
+
+    duration = time.time() - start_time
+    app.logger.info(f"[БЕЗ КЭША] Отдан список идей | Время обработки (GET): {duration:.4f} сек")
 
     return jsonify(ideas_db)
 
