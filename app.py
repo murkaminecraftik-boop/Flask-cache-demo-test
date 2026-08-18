5496283025455f8782114e50043f49e073a363bf
Murad <murad.mirzazade.emil@gmail.com>
2026-08-18 16:04:26 +0400

    Added logging

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify, render_template
 from flask_caching import Cache
 import time
 import random
-
+import logging
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
 
 app.config['CACHE_TYPE'] = 'SimpleCache'
 app.config['CACHE_DEFAULT_TIMEOUT'] = 30
@@ -17,11 +19,17 @@ def index():
 
 @app.route('/api/weather')
 def weather():
-    city = request.args.get('city', 'Unknown').lower()
+    start_time = time.time()
 
+    city = request.args.get('city', 'Unknown').lower()
     cached_data = cache.get(city)
+
     if cached_data:
         cached_data['cached'] = True
+
+        duration = time.time() - start_time
+        app.logger.info(f"[КЭШ ДЕМО] Город: {city} | Взято из кэша: ДА | Время обработки: {duration:.4f} сек")
+
         return jsonify(cached_data)
 
     time.sleep(2)
@@ -31,8 +39,11 @@ def weather():
         "temperature": random.randint(-10, 35),
         "cached": False
     }
-
     cache.set(city, data)
+
+    duration = time.time() - start_time
+    app.logger.info(f"[КЭШ ДЕМО] Город: {city} | Взято из кэша: НЕТ | Время обработки: {duration:.4f} сек")
+
     return jsonify(data)
 
 
